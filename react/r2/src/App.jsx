37d4ca32b7b14a315683b689ce20a698b3fd7b6b
Inga <inga.lingiene12@gmail.com>
2022-05-16 21:58:20 +0300

    good

--- react/r2/src/App.jsx
+++ react/r2/src/App.jsx
@@ -1,26 +1,72 @@
-
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react'; //nepamirsti useState prirasyti cia !!!
 import axios from 'axios';
 import './App.css';
-
 function App() {
-
   const [trees, setTrees] = useState([]);
-
   useEffect( () => {
     axios.get('http://localhost:3003/trees-manager')
     .then(res => {
-      console.log(res.data)
+      console.log(res.data);
+      setTrees(res.data)
     })
-  })
-  
+  },[]);
   return (
     <div className="App">
       <header className="App-header">
         <h1>Labas</h1>
+        {
+          trees.map(t => t.name !== 'Pusis' ? (<div key={t.id}>{t.name}</div>) : '')
+        }
       </header>
     </div>
   );
 }
-
 export default App;
+
+
+/*xamp start-> start-> Admin
+http://localhost/phpmyadmin/
+phpMyAdmin
+New -> irasom pavadinima i pirma laukeli gtarkim(turi buti pavadinime mazosios raides):
+sernas -> Create
+Name: trees(daugiskaita pavadinimas); Number of columns: 4 (kiek stulpeliu norim);
+GO
+
+Name     Type             Length      Anttributes        Index        A_I     
+id       MEDIUMINT                    UNSIGNED           PRIMARY     x(varnele)
+name     VARCHAR           50
+heigt    DECIMAL           4.2 (bus 4skaiciai ir du is ju po kablelio)
+type     TINYINT                      UNSIGNED
+SAVE
+
+Insert
+Column     Function    Value  
+id                    
+name                    Egle
+heigt                   8.22
+type                    2
+
+Column     Function    Value  
+id                    
+name                    Pusis
+heigt                   6.12
+type                    2
+GO (pirma o ne paskutini)
+Browse -paziuresim ka irasem
+Insert- galim dar irasyti
+
+Column     Function    Value  
+id                    
+name                    Kriause
+heigt                   1.22
+type                    1
+
+Column     Function    Value  
+id                    
+name                    Berzas
+heigt                   23.45
+type                    1
+
+(jei norim paredaguoti einam i Structure -> change(sruktura keisim); Browse ->edit (medzius paredaguoti))
+
+*/
