478701c03baa058fd4a1c448f209db1ae106708b
Inga <inga.lingiene12@gmail.com>
2022-05-19 17:16:52 +0300

    /

--- react/r2/src/Components/Front.jsx
+++ react/r2/src/Components/Front.jsx
@@ -25,7 +25,7 @@ function Front({show}) {
                 <div className="row">
                     <div className="col-12">
                         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                            <a className="navbar-brand" href="#">Tree Shop</a>
+                            <a className="navbar-brand" href="/">Tree Shop</a>
                             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                                 <span className="navbar-toggler-icon"></span>
                             </button>
