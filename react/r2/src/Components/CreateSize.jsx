22aea171b6cad663b5b276995aee66482264ce44
Inga <inga.lingiene12@gmail.com>
2022-05-28 18:57:17 +0300

    s

--- react/r2/src/Components/CreateSize.jsx
+++ react/r2/src/Components/CreateSize.jsx
@@ -1,31 +1,34 @@
-import { useState, useRef } from "react";
+import { useState } from "react";
 
 //cia Add New Tree lentele
 
-function CreateSize({ setCreateData }) { //{/*3pasiimam per cia savo setCreateData is App.jsx*/}
+function CreateSize({setCreateSizeData}) { //{/*3pasiimam per cia savo setCreateData is App.jsx*/}
     //kadangi turim 3 irasymo laukelius tai turim juos sukontroliuoti(Tree title)
     const [size, setSize] = useState('');//(Tree title-)
 
+    const sizeHandler = () => {
+        setCreateSizeData({size});
+        setSize('');
+    }
 
-        return (
-            <div className="card m-2">
-                <div className="card-header">
-                    <h2>Add New Size</h2>
+    return (
+        <div className="card m-2">
+            <div className="card-header">
+                <h2>Add New Size</h2>
+            </div>
+            <div className="card-body">
+                <div className="form-group">
+                    <label>Tree Size</label>
+                    <input type="text" className="form-control" onChange={e => setSize(e.target.value)} value={size} />
+                    <small className="form-text text-muted">Add new size name.</small>
                 </div>
-                <div className="card-body">
-                    <div className="form-group">
-                        <label>Tree title</label>
-                        <input type="text" className="form-control" onChange={e => setSize(e.target.value)} value={size} />
-                        <small className="form-text text-muted">Add new size name here.</small>
-                    </div>
-                    <div className="container p-0">
-                            <div className="buttons">
-                            <button type="button" className="btn btn-outline-primary m-3">Add</button>
-                            </div>
-                        </div>
+                <div className="container p-0">
+                    <div className="buttons">
+                        <button type="button" className="btn btn-outline-primary m-3" onClick={sizeHandler}>Add</button>
                     </div>
                 </div>
-
-        )
+            </div>
+        </div>
+    )
 }
 export default CreateSize;
